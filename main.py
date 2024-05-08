216e807e4f6a78c2b691a3f28d0996964c1d3f4a
Fernando Kenji Sakabe <33184395+fernandofks@users.noreply.github.com>
2024-05-08 17:33:45 -0300

    Colhendo mais dados

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ tmp_path = "./results/"
 new_logger = configure(tmp_path, ["stdout", "csv", "tensorboard"])
 
 env = gym.make('LunarLander-v2')
-model = DQN(policy = "MlpPolicy", env = env, tau=1.0, target_update_interval=100)
+model = DQN(policy = "MlpPolicy", env = env, tau=0.5, target_update_interval=100)
 
 model.set_logger(new_logger)
 model.learn(total_timesteps=100000)
@@ -15,7 +15,7 @@ model.learn(total_timesteps=100000)
 mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
 print(f'Mean reward: {mean_reward} +/- {std_reward:.2f}')
 
-model.save("./models/lunar_lander_1_100")
+model.save("./models/lunar_lander_01_100")
 
 print('modelo treinado')
 env = gym.make('LunarLander-v2', render_mode='human')
